1c7e9ff4eda4d553583a0b832701745d1827432b
Willie Cadete <williecadete@gmail.com>
2020-05-23 17:22:08 +0100

    Add reverse currency feature

--- app/javascript/packs/exchanges.js
+++ app/javascript/packs/exchanges.js
@@ -1,4 +1,3 @@
-
 function exchange_request() {
 
   const source_currency = document.getElementById('source_currency').value
@@ -15,20 +14,37 @@ function exchange_request() {
 
 document.addEventListener('turbolinks:load', function(){
 
-    document.getElementById('amount').addEventListener('keyup', function(event){
+    document.getElementById('amount').addEventListener('keyup', function(){
       exchange_request()
     })
 
-    document.getElementById('amount').addEventListener('change', function(event){
+    document.getElementById('amount').addEventListener('change', function(){
       exchange_request()
     })
 
-    document.getElementById('source_currency').addEventListener('change', function(event){
+    document.getElementById('source_currency').addEventListener('change', function(){
       exchange_request()
     })
 
-    document.getElementById('target_currency').addEventListener('change', function(event){
+    document.getElementById('target_currency').addEventListener('change', function(){
       exchange_request()
     })
 
+    document.getElementById('reverse').addEventListener('click', function(event){
+      event.preventDefault()
+
+      const source = document.getElementById('source_currency')
+      const target = document.getElementById('target_currency')
+      
+      const source_currency = target.options[target.options.selectedIndex] 
+      const target_currency = source.options[source.options.selectedIndex]
+
+      source.value = source_currency.value
+      source.text = source_currency.text
+
+      target.value = target_currency.value
+      target.text = target_currency.value
+      
+      exchange_request()
+    })
 })
