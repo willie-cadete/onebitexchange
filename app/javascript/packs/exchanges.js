5db733b6ed3884a79657963731f0a2bc5bc54774
Willie Cadete <williecadete@gmail.com>
2020-05-23 07:00:34 +0100

    Remove submit button

--- app/javascript/packs/exchanges.js
+++ app/javascript/packs/exchanges.js
@@ -1,6 +1,34 @@
-document.addEventListener('turbolinks:load', function(){
-  document.getElementById('exchange_form').addEventListener('ajax:success', function(event) {
-    let [result] = event.detail
-    document.getElementById('result').value = result.value
+
+function exchange_request() {
+
+  const source_currency = document.getElementById('source_currency').value
+  const target_currency = document.getElementById('target_currency').value
+  const amount = document.getElementById('amount').value
+
+  fetch(`/convert?source_currency=${source_currency}&amount=${amount}&target_currency=${target_currency}`)
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('result').value = parseFloat(data.value).toFixed(2)
   })
+
+}
+
+document.addEventListener('turbolinks:load', function(){
+
+    document.getElementById('amount').addEventListener('keyup', function(event){
+      exchange_request()
+    })
+
+    document.getElementById('amount').addEventListener('change', function(event){
+      exchange_request()
+    })
+
+    document.getElementById('source_currency').addEventListener('change', function(event){
+      exchange_request()
+    })
+
+    document.getElementById('target_currency').addEventListener('change', function(event){
+      exchange_request()
+    })
+
 })
